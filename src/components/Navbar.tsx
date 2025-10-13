0c9bc3a9fd5d937fc8f768dc00c88ea9dd0c3092
sandersal <49814328+sandersal@users.noreply.github.com>
2025-10-13 18:45:03 -0300

    Botão Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,13 +46,14 @@ const Navbar = () => {
               >
                 {link.name}
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-gradient-to-r from-primary to-secondary transition-all group-hover:w-full" />
-
-                <a href="https://wa.me/5593999999999?text=Olá!%20Gostaria%20de%20solicitar%20um%20orçamento."
-                target="_blank"
-                rel="noopener noreferrer"
-                >
               </a>
             ))}
+             {/* Botão WhatsApp */}
+                <a
+                  href="https://wa.me/5593991872819?text=Olá!%20Gostaria%20de%20solicitar%20um%20orçamento."
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
             <Button variant="hero" size="default">
               Solicitar Orçamento
             </Button>
